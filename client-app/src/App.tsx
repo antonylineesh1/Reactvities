c94ea2711a39cbc43ed080676bc5be2cc8b12f5d
antonylineesh1 <antonylineesh1@gmail.com>
2020-09-15 00:18:18 +0530

    End of section

--- client-app/src/App.tsx
+++ client-app/src/App.tsx
@@ -3,33 +3,33 @@ import logo from "./logo.svg";
 import "./App.css";
 import { cars } from "./demo";
 import { CarItem } from "./CarItem";
-import axios from 'axios';
+import axios from "axios";
+import { Header, Icon, List } from "semantic-ui-react";
 
 class App extends Component {
-  state={
-    values:[]
+  state = {
+    values: [],
+  };
+  componentDidMount() {
+    axios.get("http://localhost:5000/api/values").then((response) => {
+      
+      this.setState({ values: response.data });
+    });
   }
-  componentDidMount()
-  {
-    axios.get('http://localhost:5000/api/values').then(response=>{
-    
-      console.log(response);
-      this.setState({values:response.data});
-
-    })
-    
-  }
-  render(){
+  render() {
     return (
-      <div className="App">
-        <ul>
-          {/* {cars.map((car,index) => (
-            <CarItem key={index} car={car}/>
-          ))} */}          
-          {
-            this.state.values.map((value:any)=><li key={value.id}>{value.name}</li>)
-          }
-        </ul>
+      <div>
+        <Header as="h2">
+          <Icon name="plug" />
+          <Header.Content>reactivities</Header.Content>
+        </Header>
+        <List>
+          {this.state.values.map((value: any) => (
+            <List.Item key={value.id}>{value.name}</List.Item>
+          ))}
+        </List>
+
+        <ul></ul>
       </div>
     );
   }
