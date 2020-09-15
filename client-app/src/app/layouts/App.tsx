4f7baabf70699a62f5a0dfe7c80e8c72fa5453b0
antonylineesh1 <antonylineesh1@gmail.com>
2020-09-15 19:16:33 +0530

    section-5 refactor class components to use react hooks

--- client-app/src/app/layouts/App.tsx
+++ client-app/src/app/layouts/App.tsx
@@ -1,43 +1,36 @@
-import React, { Component } from "react";
+import React, { useEffect, useState } from "react";
 import logo from "./logo.svg";
 import { CarItem } from "../../CarItem";
 import axios from "axios";
 import { Header, Icon, List } from "semantic-ui-react";
 import { IActivity } from "../models/IActivity";
 
-interface IState {
-  activities: IActivity[];
-}
+const App = () => {
+  const [activities, setActivities] = useState<IActivity[]>([]);
 
-class App extends Component {
-  readonly state: IState = {
-    activities: [],
-  };
+  useEffect(() => {
+    axios
+      .get<IActivity[]>("http://localhost:5000/api/activities")
+      .then((response) => {
+        setActivities(response.data);
+      });
+  }, []);
 
-  componentDidMount() 
-  {
-    axios.get<IActivity[]>("http://localhost:5000/api/activities").then((response) => {
-      this.setState({ activities: response.data });
-    });
+  return (
+    <div>
+      <Header as="h2">
+        <Icon name="plug" />
+        <Header.Content>reactivities</Header.Content>
+      </Header>
+      <List>
+        {activities.map((value) => (
+          <List.Item key={value.id}>{value.title}</List.Item>
+        ))}
+      </List>
 
-  }
-  render() {
-    return (
-      <div>
-        <Header as="h2">
-          <Icon name="plug" />
-          <Header.Content>reactivities</Header.Content>
-        </Header>
-        <List>
-          {this.state.activities.map((value) => (
-            <List.Item key={value.id}>{value.title}</List.Item>
-          ))}
-        </List>
-
-        <ul></ul>
-      </div>
-    );
-  }
-}
+      <ul></ul>
+    </div>
+  );
+};
 
 export default App;
