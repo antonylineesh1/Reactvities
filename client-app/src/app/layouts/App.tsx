ee9b8afd70a96f45637d6dcce2db5950d757f071
antonylineesh1 <antonylineesh1@gmail.com>
2020-09-15 19:34:52 +0530

    Section-5 adding navigation bar

--- client-app/src/app/layouts/App.tsx
+++ client-app/src/app/layouts/App.tsx
@@ -1,9 +1,9 @@
 import React, { useEffect, useState } from "react";
 import logo from "./logo.svg";
-import { CarItem } from "../../CarItem";
 import axios from "axios";
 import { Header, Icon, List } from "semantic-ui-react";
 import { IActivity } from "../models/IActivity";
+import { Nav } from "../../features/nav/Nav";
 
 const App = () => {
   const [activities, setActivities] = useState<IActivity[]>([]);
@@ -18,10 +18,7 @@ const App = () => {
 
   return (
     <div>
-      <Header as="h2">
-        <Icon name="plug" />
-        <Header.Content>reactivities</Header.Content>
-      </Header>
+      <Nav/>
       <List>
         {activities.map((value) => (
           <List.Item key={value.id}>{value.title}</List.Item>
