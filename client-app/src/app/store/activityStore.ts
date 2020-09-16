171cbea8945372a6cfef0226bc1ae89214bd4508
antonylineesh1 <antonylineesh1@gmail.com>
2020-09-17 01:28:33 +0530

    Section-7 84. Using Async Await in our methods

--- client-app/src/app/store/activityStore.ts
+++ client-app/src/app/store/activityStore.ts
@@ -6,32 +6,33 @@ import { IActivity } from "../models/IActivity";
 class ActivityStore {
   @observable activities: IActivity[] = [];
   @observable loadingInitial = false;
-  @observable editMode=false;
-  @observable selectedActivity:IActivity|undefined;
+  @observable editMode = false;
+  @observable selectedActivity: IActivity | undefined;
 
-
-  @action selectActivity=(id:string)=>
-  {
-   this.selectedActivity=this.activities.find(actv=>actv.id===id);
-   this.editMode=false;     
-  }
-
-  @action loadActivities = () => {
-    this.loadingInitial = true;
-
-    agent.Activities.list()
-      .then((activities) => {
-        activities.forEach((activity) => {
-          activity.date = activity.date.split(".")[0];
-          this.activities.push(activity);
-        });
-      })
-      .finally(() => {
-        this.loadingInitial = false;
-      });
+  @action selectActivity = (id: string) => {
+    this.selectedActivity = this.activities.find((actv) => actv.id === id);
+    this.editMode = false;
   };
 
+  @action loadActivities = async () => {
+    this.loadingInitial = true;
 
+    try 
+    {
+      const activities = await agent.Activities.list();
+      activities.forEach((activity) => {
+        activity.date = activity.date.split(".")[0];
+        this.activities.push(activity);
+      });
+      this.loadingInitial = false;
+    }
+    catch (error) 
+    {
+      console.log(error);
+      this.loadingInitial = false;
+    }
+    
+  };
 }
 
 export default createContext(new ActivityStore());
