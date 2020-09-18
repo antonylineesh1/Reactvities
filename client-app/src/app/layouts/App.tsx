756339d5eddff424669f8ef6c37ebd2baa7f4b8a
antonylineesh1 <antonylineesh1@gmail.com>
2020-09-18 11:15:31 +0530

    105. Moving the home page outside of our navigation routes

--- client-app/src/app/layouts/App.tsx
+++ client-app/src/app/layouts/App.tsx
@@ -10,7 +10,7 @@ import { HomePage } from "../../features/home/HomePage";
 import { ActivityForm } from "../../features/activities/form/ActivityForm";
 import ActivityDetails from "../../features/activities/details/ActivityDetails";
 
-const App : React.FC<RouteComponentProps> = ({location}) => {
+const App: React.FC<RouteComponentProps> = ({ location }) => {
   const activityStore = useContext(ActivityStore);
 
   useEffect(() => {
@@ -21,13 +21,26 @@ const App : React.FC<RouteComponentProps> = ({location}) => {
     return <LoadingComponent content="Loading activities" />;
   return (
     <Fragment>
-      <Nav />
-      <Container style={{ marginTop: "7em" }}>
-        <Route exact path="/" component={HomePage} />
-        <Route  path="/activities" component={ActivityDashboard} />
-        <Route exact  path="/activity/:id" component={ActivityDetails} />
-        <Route key={location.key} exact path={["/createForm","/manage/:id"]} component={ActivityForm} />
-      </Container>
+      <Route exact path="/" component={HomePage} />
+
+      <Route
+        path={"/(.+)"}
+        render={() => (
+          <Fragment>
+            <Nav />
+            <Container style={{ marginTop: "7em" }}>
+              <Route path="/activities" component={ActivityDashboard} />
+              <Route exact path="/activity/:id" component={ActivityDetails} />
+              <Route
+                key={location.key}
+                exact
+                path={["/createForm", "/manage/:id"]}
+                component={ActivityForm}
+              />
+            </Container>
+          </Fragment>
+        )}
+      />
     </Fragment>
   );
 };
--- client-app/src/features/home/HomePage.tsx
+++ client-app/src/features/home/HomePage.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
 import { Container } from 'semantic-ui-react'
 
 export const HomePage = () => {
     return (
         <Container style={{marginTop:'10px'}}>
             <h1>This is home page</h1>
+            <h3>Go to <NavLink to='/activities'>actitivities</NavLink></h3> 
         </Container>
     )
 }
